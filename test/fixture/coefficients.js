df1b6936f00527c7241ca50fa8d615d88bf7bba5
kavi4 <antoniy986@gmail.com>
2020-11-07 15:47:05 +0300

    define fixtures units and coefficients

---
+++ test/fixture/coefficients.js
@@ -0,0 +1,12 @@
+import Units from "./units";
+import CoefficientMap from '../../src/CoefficientMap'
+
+export default new CoefficientMap([
+    [Units.KILO, 1000],
+    [Units.HECTO, 100],
+    [Units.DECA, 10],
+    [Units.SOURCE, 1],
+    [Units.DECI, 0.1],
+    [Units.CENTI, 0.01],
+    [Units.MILLI, 0.001],
+])
---
+++ test/fixture/units.js
@@ -0,0 +1,9 @@
+export default {
+    KILO: 'kilo',
+    HECTO: 'hecto',
+    DECA: 'deca',
+    SOURCE: 'source',
+    DECI: 'deci',
+    CENTI: 'centi',
+    MILLI: 'milli',
+}
